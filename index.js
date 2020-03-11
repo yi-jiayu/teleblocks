9ebdebfd8458999bb8df63a50d2648a6de4451b6
Jiayu Yi <yijiayu@gmail.com>
2020-03-11 22:46:57 +0800

    Add Telegram-specific blocks

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import 'blockly/blocks';
 import 'blockly/python';
 import * as En from 'blockly/msg/en';
 
+import blocks from './blocks.json';
+
 Blockly.setLocale(En);
+
+Blockly.defineBlocksWithJsonArray(blocks);
 
 document.addEventListener('DOMContentLoaded', function () {
   const workspace = Blockly.inject('blocklyDiv',
