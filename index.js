3e62013dea1ce5f24652bda91c7e000a01bc5e43
Jiayu Yi <yijiayu@gmail.com>
2020-03-11 23:54:12 +0800

    Remove code generation for now

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import Blockly from 'blockly/core';
 import 'blockly/blocks';
-import 'blockly/python';
 import * as En from 'blockly/msg/en';
 
 import blocks from './blocks.json';
@@ -12,10 +11,4 @@ Blockly.defineBlocksWithJsonArray(blocks);
 document.addEventListener('DOMContentLoaded', function () {
   const workspace = Blockly.inject('blocklyDiv',
       {toolbox: document.getElementById('toolbox')});
-
-  const generateCode = function () {
-    document.getElementById('output').textContent = Blockly['Python'].workspaceToCode(workspace);
-  };
-
-  workspace.addChangeListener(generateCode);
 });
